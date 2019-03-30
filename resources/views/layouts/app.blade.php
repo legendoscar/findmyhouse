f0ae2d513bd6c0d65672392ddecbf55666b5568c
rexsteroxy <rexsteroxy22@gmail.com>
2019-03-30 20:49:44 +0100

    Added the profile,location,property controllers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,9 @@
 
                                 <ul class="dropdown-menu">
                                     <li>
+                                    <li><a href="{{url('/profile')}}">Create Profile</a></li>
+                                    <li><a href="{{url('/location')}}">Add Location</a></li>
+                                    <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
